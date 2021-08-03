4871cea8b3f9b1e627264be0c140e597d7656586
Wesley-Kmelo <wesleycamelo@gmail.com>
2021-08-03 11:03:06 -0300

    Primeira atualização de programa

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,11 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+       // System.out.println("Hello World!!!");
+        Gato gato = new Gato("jones","azul",12);
+        System.out.println(gato);
+    }
+}
